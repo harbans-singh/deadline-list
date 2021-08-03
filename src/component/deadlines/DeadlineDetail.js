f01585d5e994448682b94cc00891999a550ea424
Harbans Singh <harbans.tech@gmail.com>
2021-08-03 14:47:46 +0530

    Code before authentication

--- src/component/deadlines/DeadlineDetail.js
+++ src/component/deadlines/DeadlineDetail.js
@@ -3,38 +3,61 @@ import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import DateObject from "react-date-object";
 import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    btn: {
+        backgroundColor: '#436185',
+        marginTop: 20,
+        '&:hover': {
+            backgroundColor: '#293852'
+        }
+    }
+})
 
 const DeadlineDetail = (props) => {
 
     const deadline = props.deadline;
-    
-    return (
-        <div className="deadline-detail-container">
-            <div className="deadline-detail">
-                <div className="deadline-detail-title">
-                    {deadline && deadline.title}
-                </div>
-                <div className="deadline-detail-body">
-                    <p>{deadline && deadline.detail}</p>
-                </div>
-                <div className="deadline-detail-footer">
-                    <div className="deadline-detail-date">
-                        {deadline && ("Added on: " + (new DateObject(deadline.createdAt.seconds * 1000)).format("DD MMMM YYYY"))}
+    const classes = useStyles();
+
+    if(deadline) {
+        return (
+            <div className="deadline-detail-container">
+                <div className="deadline-detail">
+                    <div className="deadline-detail-title">
+                        {deadline.title}
                     </div>
-                    <div className="deadline-detail-time">
-                        {deadline && ("At: " + (new DateObject(deadline.createdAt.seconds * 1000)).format("hh:mm a"))}
+                    <div className="deadline-detail-body">
+                        <p>{deadline.detail}</p>
                     </div>
-                    <div className="deadline-detail-delete-btn">
-                        <Button
-                        variant="contained"
-                        color="primary">
-                            Delete
-                        </Button>
+                    <div className="deadline-detail-footer">
+                        <div className="deadline-detail-date">
+                            {"Added on: " + (new DateObject(deadline.createdAt.seconds * 1000)).format("DD MMMM YYYY")}
+                        </div>
+                        <div className="deadline-detail-time">
+                            {"At: " + (new DateObject(deadline.createdAt.seconds * 1000)).format("hh:mm a")}
+                        </div>
+                        <div className="deadline-detail-delete-btn"> 
+                            <Button
+                            className={classes.btn}
+                            variant="contained"
+                            color="primary">
+                                DELETE
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else {
+        return (
+            <div className="deadline-detail-container">
+                <p>Loading...</p>
+            </div>
+        );
+    }
+    
 }
 
 const mapStateToProps = (state) => {
